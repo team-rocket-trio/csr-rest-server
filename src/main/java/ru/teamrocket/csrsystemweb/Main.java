1fce318416e64f059a302fbb8bcf87af2849fc55
kotofeyskaya <catherine.kh@ya.ru>
2017-01-25 22:41:15 +0400

    first commit

---
+++ src/main/java/ru/teamrocket/csrsystemweb/Main.java
@@ -0,0 +1,20 @@
+package ru.teamrocket.csrsystemweb;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Kate on 25.01.2017.
+ */
+
+@Path("/hello")
+public class Main {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getMessage(){
+        return Response.status(200).entity("hello hello").build();
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Kate
+  Date: 25.01.2017
+  Time: 0:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
